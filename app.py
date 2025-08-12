220415f44580614d3e60f3dab8d09e8a804a2b9e
Ferrari-Cassiano <223904719+Ferrari-Cassiano@users.noreply.github.com>
2025-08-12 13:24:49 -0300

    feat: Adiciona tabela de resultados com st.session_state

--- app.py
+++ app.py
@@ -1,41 +1,54 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
 
+# estas são variáveis persistentes preservadas à medida que o Streamlit executa novamente esse script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
+
 st.header('Jogando uma moeda')
 
-# Inicializa o gráfico de linhas com um valor inicial
 chart = st.line_chart([0.5])
 
 def toss_coin(n):
-    # Simula n lançamentos de moeda (0 = coroa, 1 = cara)
     trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
 
     mean = None
     outcome_no = 0
     outcome_1_count = 0
 
-    # Itera sobre os resultados, calcula a média e atualiza o gráfico
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
-        # Adiciona uma nova linha de dados ao gráfico
         chart.add_rows([mean])
-        # Pequeno atraso para visualizar o progresso
         time.sleep(0.05)
 
     return mean
 
-# Widget de controle deslizante para o número de tentativas
 number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
-# Botão para iniciar a simulação
 start_button = st.button('Executar')
 
-# Lógica executada quando o botão é clicado
 if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
-    # Chama a função de lançamento da moeda
+    st.session_state['experiment_no'] += 1 # Incrementa o número do experimento
     mean = toss_coin(number_of_trials)
-    # Opcional: Você pode adicionar aqui um st.write(f'Média final: {mean}') se quiser mostrar o resultado final.
+    # Adiciona o resultado do experimento ao DataFrame no estado da sessão
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+# Exibe o DataFrame com os resultados de todos os experimentos
+st.write(st.session_state['df_experiment_results'])
